b201a7ae0ffc485a3530c72539dc50e70e28857d
tim.rohrer <tim@tgr66.dev>
2023-01-29 17:46:49 -0600

    feat: Add accounts to GnuCash book.
    Accounts to add come from a list of account objects.
    See fixture for format.

--- src/move2gnucash/file_operations.py
+++ src/move2gnucash/file_operations.py
@@ -1,5 +1,34 @@
+"""
+Contains the functions that operate on csv and GnuCash files.
+"""
 import pandas as pd
+
+from piecash import Account, Book, create_book
+from piecash.core.factories import create_currency_from_ISO
+
 
 def fetch_csv_data(file_to_open):
     """Read all csv contents of file and return DataFrame"""
     return pd.read_csv(file_to_open)
+
+
+def create_gnucash_book(filename: str, currency_str: str = "USD", overwrite=False):
+    """Wrapper to create_book with select defaults."""
+    create_book(filename, currency=create_currency_from_ISO(currency_str), overwrite=overwrite)
+
+
+def create_accounts(book: Book, accounts_to_add):
+    """Add accounts and save book."""
+    for account in accounts_to_add:
+        name, account_type, parent, commodity, placeholder = account.values()
+        parent_acct = book.accounts(fullname=parent) if parent != "root" else book.root_account
+        Account(
+            parent=parent_acct,
+            name=name,
+            type=account_type,
+            commodity=book.commodities(mnemonic=commodity),
+            placeholder=placeholder,
+        )
+        book.flush()
+
+    book.save()
--- tests/test_file_operations.py
+++ tests/test_file_operations.py
@@ -1,10 +1,10 @@
+"""test_file_operations.py"""
 from unittest.mock import patch, Mock
 
-import pandas as pd
+from piecash import create_book
 
-# import move2gnucash.file_operations
-from move2gnucash.file_operations import fetch_csv_data
-# from .move2gnucash.file_operations import fetch_csv_data
+from move2gnucash.file_operations import fetch_csv_data, create_gnucash_book, create_accounts
+
 
 @patch("pandas.read_csv")
 def test_fetch_csv_data_works(mock_read: Mock):
@@ -13,10 +13,80 @@ def test_fetch_csv_data_works(mock_read: Mock):
     WHEN executed
     THEN a Pandas DataFrame is returned with the data
     """
-
-    df = pd.DataFrame({"foo_id": [1, 2, 3, 4, 5]})
-
-    results = fetch_csv_data("file")
+    fetch_csv_data("file")
 
     mock_read.assert_called_once_with("file")
-    # pd.testing.assert_frame_equal(results, df)
+
+
+@patch("move2gnucash.file_operations.create_book")
+def test_create_gnucash_book(mock_create_book: Mock):
+    """
+    GIVEN the proper parameters, minimally a filename
+    WHEN passed to create_gnucash_book and executed
+    THEN piecash.create_book will be called with all of the parameters.
+    """
+    create_gnucash_book("testfile")
+
+    mock_create_book.assert_called_once_with("testfile")
+
+
+def test_add_accounts_success(fixture_accounts):
+    """
+    GIVEN a list of five accounts and sub accounts, and a viable book instance
+    WHEN executed with add_accounts
+    THEN five new accounts will be added, if unique.
+    """
+    book = create_book(currency="USD")
+
+    create_accounts(book, fixture_accounts)
+
+    assert len(book.accounts) == 5
+
+
+def test_add_accounts_success_correct_tree(fixture_accounts):
+    """
+    GIVEN an array of accounts and sub accounts, and a viable book instance
+    WHEN executed with add_accounts,
+    THEN the list of accounts will have the correct parent/child structure
+    """
+    book = create_book(currency="USD")
+
+    create_accounts(book, fixture_accounts)
+
+    added_accounts = list(map(lambda acct: acct.fullname, book.accounts))
+    assert "Assets:Current Assets" in added_accounts
+    assert book.accounts(fullname="Assets:Current Assets").parent.name == "Assets"
+    assert "Assets:Current Assets:Checking" in added_accounts
+
+
+# def test_add_transactions_success():
+#     """
+#     GIVEN a book with a complete chart of accounts, and a list of transactions
+#     WHEN executed with create_transactions
+#     THEN the book will contain accounts and transactions.
+#     """
+#     today = datetime.now()
+#     book = create_book(currency="USD")
+#     book.root_account.children = accounts_fixture(book)
+#     book.save()
+#     to_account = book.accounts(fullname="Assets:Current Assets:Checking")
+#     from_account = book.accounts(fullname="Equity:Opening Balances")
+#     amount = Decimal("2350.47")
+#     transactions = [
+#         Transaction(
+#             post_date=today.date(),
+#             enter_date=today,
+#             currency=book.commodities(mnemonic="USD"),
+#             description="Test Transaction",
+#             splits=[
+#                 Split(account=to_account, value=amount, memo="Split Memo!"),
+#                 Split(account=from_account, value=-amount, memo="Other Split Memo!"),
+#             ],
+#         )
+#     ]
+
+#     add_transactions(book, transactions)
+
+#     account_to_check = book.accounts(fullname="Assets:Current Assets:Checking")
+#     assert account_to_check.get_balance() == amount
+#     assert from_account.get_balance() == amount * -1
